6dfab935ad89f7bbf837926b4d4e3c15eec80e83
Thales Menezes <thlmenezes@pm.me>
2022-02-19 15:31:43 -0300

    🔧 eslint customize default

--- packages/config/eslint-preset.js
+++ packages/config/eslint-preset.js
@@ -1,11 +1,44 @@
 module.exports = {
-  extends: ["next", "prettier"],
+  extends: ["next", "universe/web"],
   settings: {
     next: {
       rootDir: ["apps/*/", "packages/*/"],
     },
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
+    },
+    "import/resolver": {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
   },
+  plugins: ["import", "eslint-plugin-import-helpers"],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
+    "import/namespace": [
+      "error",
+      {
+        allowComputed: true,
+      },
+    ],
+    camelcase: "error",
+    "import/no-unresolved": "error",
+    "import-helpers/order-imports": [
+      "error",
+      {
+        newlinesBetween: "always",
+        groups: ["module", ["/^~/"], ["parent", "sibling", "index"]],
+        alphabetize: {
+          order: "asc",
+          ignoreCase: true,
+        },
+      },
+    ],
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "no-use-before-define": "off",
+    "@typescript-eslint/no-use-before-define": "off",
+    "@typescript-eslint/no-empty-function": "warn",
+    "@typescript-eslint/ban-ts-comment": "warn",
   },
 };
