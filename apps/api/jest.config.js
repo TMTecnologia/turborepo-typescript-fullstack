3f82ce0cfb18dfbf977efd06468d73063f9b6e5d
Thales Menezes <thlmenezes@pm.me>
2022-03-09 00:51:28 -0300

    🔧 jest with module mapping

---
+++ apps/api/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  moduleFileExtensions: ["js", "json", "ts"],
+  rootDir: "src",
+  testRegex: ".*\\.spec\\.ts$",
+  transform: {
+    "^.+\\.(j|t)s$": "ts-jest",
+  },
+  collectCoverageFrom: ["**/*.(t|j)s"],
+  coverageDirectory: "../coverage",
+  testEnvironment: "node",
+  moduleNameMapper: {
+    "^~/(.*)$": "<rootDir>/$1",
+  },
+  coverageReporters: ["json-summary", "text", "lcov"],
+};
