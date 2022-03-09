4951c7bf53dac94b1b36e6cf4634a461e82e12f2
Thales Menezes <thlmenezes@pm.me>
2022-03-08 22:35:23 -0300

    ✨ prisma service initialize and shutdown

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from "@nestjs/core";
 import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 
 import { AppModule } from "./app.module";
+import { PrismaService } from "./prisma.service";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,9 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("api", app, document);
+
+  const prismaService: PrismaService = app.get(PrismaService);
+  prismaService.enableShutdownHooks(app);
 
   await app.listen(3000);
 }
