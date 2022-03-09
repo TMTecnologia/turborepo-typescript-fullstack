eef1b3ddce34ec953be6c3062527e2d80db5ad6d
Thales Menezes <thlmenezes@pm.me>
2022-03-08 21:36:56 -0300

    ✨ nest g products

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from "@nestjs/common";
 
+import { ProductsModule } from "./products/products.module";
+
 @Module({
-  imports: [],
+  imports: [ProductsModule],
   controllers: [],
   providers: [],
 })
