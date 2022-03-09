f094833cd85f6288f1fed06f286545a00880d033
Thales Menezes <thlmenezes@pm.me>
2022-03-09 00:55:27 -0300

    ✨ e2e test expectation

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from "@nestjs/common";
 
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
 import { ProductsModule } from "./products/products.module";
 
 @Module({
   imports: [ProductsModule],
-  controllers: [],
-  providers: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
