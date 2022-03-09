542c742dbaff2f13af09065907336fd28ef267a4
Thales Menezes <thlmenezes@pm.me>
2022-03-08 23:54:04 -0300

    ✨ create-product-dto + validation

--- apps/api/src/products/dto/create-product.dto.ts
+++ apps/api/src/products/dto/create-product.dto.ts
@@ -1,1 +1,12 @@
-export class CreateProductDto {}
+import { Prisma } from "@prisma/client";
+import { IsOptional, IsString, IsCurrency } from "class-validator";
+
+export class CreateProductDto {
+  @IsString()
+  name: string;
+  @IsCurrency()
+  price: Prisma.Decimal | number | string;
+  @IsString()
+  @IsOptional()
+  description?: string;
+}
