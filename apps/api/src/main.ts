fd3669cae981b4bbabeb2fcb24bd396ca14cc03a
Thales Menezes <thlmenezes@pm.me>
2022-03-09 21:12:15 -0300

    🚀 heroku needs to see PORT connect

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -19,6 +19,6 @@ async function bootstrap() {
   const prismaService: PrismaService = app.get(PrismaService);
   prismaService.enableShutdownHooks(app);
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
